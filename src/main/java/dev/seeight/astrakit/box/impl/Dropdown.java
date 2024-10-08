d59dfbc51cdb335da665ec8e8074c5fcefbe4973
C8FF <theprestigefox@googlemail.com>
2024-10-08 14:35:20 -0600

    Add scrollOver method to Dropdown

--- src/main/java/dev/seeight/astrakit/box/impl/Dropdown.java
+++ src/main/java/dev/seeight/astrakit/box/impl/Dropdown.java
@@ -1,8 +1,9 @@
 package dev.seeight.astrakit.box.impl;
 
 public interface Dropdown {
-
 	void renderOver(float x, float y, float alpha);
 
 	void mouseEventOver(int button, int action, double mx, double my);
+
+	void scrollOver(double x, double y);
 }
